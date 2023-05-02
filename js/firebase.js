7c1e983b2ff825bfd486838673751c660befafc6
co1umb <66077930+co1umb@users.noreply.github.com>
2023-05-02 11:10:53 +0300

    Initial commit

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVAYgplPsMjChhB0-K2oXxqBnirKO9JeM",
+  authDomain: "auctionua.firebaseapp.com",
+  projectId: "auctionua",
+  storageBucket: "auctionua.appspot.com",
+  messagingSenderId: "1061440105729",
+  appId: "1:1061440105729:web:b8ab6d758c7dcb29e33f00"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
